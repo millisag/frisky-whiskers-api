8c40d9d20bd183bef17f5d9c260f6c577812d7cd
Millisa G <millisa419@yahoo.com>
2025-09-21 15:53:07 -0400

    fixed routes to enable sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
+# Rails.application.routes.draw do
+#   # Login
+#   post '/login', to: 'sessions#create'
+
+#   # Users and their cats
+  
+#     resources :cats do
+#       resources :food_entries, only: [:index, :create, :show, :update, :destroy]
+#       resources :activity_entries, only: [:index, :create, :show, :update, :destroy]
+#     end
+#   end
 Rails.application.routes.draw do
   # Login
   post '/login', to: 'sessions#create'
 
-  # Users and their cats
-  
-    resources :cats do
-      resources :food_entries, only: [:index, :create, :show, :update, :destroy]
-      resources :activity_entries, only: [:index, :create, :show, :update, :destroy]
-    end
+  # Users
+  resources :users, only: [:create]
+
+  # Users' cats and their entries
+  resources :cats do
+    resources :food_entries, only: [:index, :create, :show, :update, :destroy]
+    resources :activity_entries, only: [:index, :create, :show, :update, :destroy]
   end
+end
 
 
